ba4783e5e76905195bd6b0e35e8877f486809913
luisarita <github@luisarita.com>
2017-08-14 10:09:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  }
+}
+ 
+var pbj = new Sandwich("wheat", ["chunky peanut butter", "blackberry preserves"], "PB&Jam");
+ 
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+}
+ 
+salad.describe = pbj.describe.bind(salad);
+
+//create new Customer instance
+var sally = new Customer("Sally", "4");
+ 
+//schedule table visit
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
